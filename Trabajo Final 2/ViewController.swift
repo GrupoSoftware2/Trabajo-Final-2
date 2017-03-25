6bf2d4b35efe0ae0be7fc204406bacf0d1aba68a
Rocío Córdova <stefnova@la-iMac-de-Rocio.local>
2017-03-25 16:07:57 -0500

    Initial Commit

---
+++ Trabajo Final 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trabajo Final 2
+//
+//  Created by Rocío Córdova on 25/03/17.
+//  Copyright © 2017 José Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
